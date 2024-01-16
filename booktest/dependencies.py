445faef7f6fd33c2e54cc94e58397b6cd4eba864
Antti Rauhala <antti@lumoa.me>
2024-01-16 10:52:10 +0200

    documentation updated.

--- booktest/dependencies.py
+++ booktest/dependencies.py
@@ -4,8 +4,21 @@ import inspect
 
 class Resource:
 
-    def __init__(self, identifier):
+    def __init__(self, value, identifier=None):
+        self.value = value
+        if identifier is None:
+            identifier = value
         self.identifier = identifier
+
+    def __eq__(self, other):
+        return isinstance(other, Resource) and self.identifier == other.identifier
+
+    def __hash__(self):
+        return hash(self.identifier)
+
+
+def port(value: int):
+    return Resource(value, f"port={value}")
 
 
 def bind_dependent_method_if_unbound(method, dependency):
@@ -21,36 +34,40 @@ def bind_dependent_method_if_unbound(method, dependency):
         return dependency
 
 
-def call_class_method_test(methods, func, self, case, kwargs):
+def call_class_method_test(dependencies, func, self, case, kwargs):
 
     args2 = []
     args2.append(self)
     args2.append(case)
 
     run = case.run
-    for unbound_method in methods:
-        # 1. Try first to find this method for this exact test instance.
-        #    This covers cases, where a test class has been instantiated
-        #    with several different parameters
+    for dependency in dependencies:
+        if isinstance(dependency, Resource):
+            args2.append(dependency.value)
+        else:
+            unbound_method = dependency
+            # 1. Try first to find this method for this exact test instance.
+            #    This covers cases, where a test class has been instantiated
+            #    with several different parameters
 
-        bound_method = unbound_method.__get__(self, self.__class__)
-        found, result = \
-            run.get_test_result(
-                case,
-                bound_method)
-
-        # 2. If method is not exist for test instance, try to look elsewhere.
-        #    This allows for tests to share same data or prepared model
-        if not found:
+            bound_method = unbound_method.__get__(self, self.__class__)
             found, result = \
                 run.get_test_result(
                     case,
-                    unbound_method)
+                    bound_method)
 
-        if not found:
-            raise ValueError(f"could not find or make method {unbound_method} result")
+            # 2. If method is not exist for test instance, try to look elsewhere.
+            #    This allows for tests to share same data or prepared model
+            if not found:
+                found, result = \
+                    run.get_test_result(
+                        case,
+                        unbound_method)
 
-        args2.append(result)
+            if not found:
+                raise ValueError(f"could not find or make method {unbound_method} result")
+
+            args2.append(result)
 
     return func(*args2, **kwargs)
 
@@ -61,16 +78,19 @@ def call_function_test(methods, func, case, kwargs):
     args2 = []
     args2.append(case)
 
-    for unbound_method in methods:
-        found, result = \
-            run.get_test_result(
-                case,
-                unbound_method)
+    for dependency in methods:
+        if isinstance(dependency, Resource):
+            args2.append(dependency.value)
+        else:
+            found, result = \
+                run.get_test_result(
+                    case,
+                    dependency)
 
-        if not found:
-            raise ValueError(f"could not find or make method {unbound_method} result")
+            if not found:
+                raise ValueError(f"could not find or make method {dependency} result")
 
-        args2.append(result)
+            args2.append(result)
 
     return func(*args2, **kwargs)
 
@@ -83,7 +103,7 @@ def depends_on(*dependencies):
     resources = []
     for i in dependencies:
         if isinstance(i, Resource):
-            resources.append(i.identifier)
+            resources.append(i)
         else:
             methods.append(i)
 
@@ -93,9 +113,9 @@ def depends_on(*dependencies):
             from booktest import TestBook
 
             if isinstance(args[0], TestBook):
-                return call_class_method_test(methods, func, args[0], args[1], kwargs)
+                return call_class_method_test(dependencies, func, args[0], args[1], kwargs)
             else:
-                return call_function_test(methods, func, args[0], kwargs)
+                return call_function_test(dependencies, func, args[0], kwargs)
 
         wrapper._dependencies = methods
         wrapper._resources = resources
--- test/examples/resource_book.py
+++ test/examples/resource_book.py
@@ -1,14 +1,18 @@
 from time import sleep
 
 import booktest as bt
-from booktest.dependencies import Resource
 
 
-THE_GLOBAL_VALUE = 1
+class Box:
+
+    def __init__(self, value):
+        self.value = value
 
 
-def t_resource_use_with_race_condition(t: bt.TestCaseRun):
-    global THE_GLOBAL_VALUE
+THE_GLOBAL_BOX = Box(1)
+
+
+def t_resource_use_with_race_condition(t: bt.TestCaseRun, global_box):
     t.h1("description:")
     t.tln("this test is run several times separately")
     t.tln("there will be race condition, if run parallel")
@@ -16,28 +20,28 @@ def t_resource_use_with_race_condition(t: bt.TestCaseRun):
 
     t.h1("test sequence:")
 
-    t.tln(f" * the global value is {THE_GLOBAL_VALUE}")
-    THE_GLOBAL_VALUE = THE_GLOBAL_VALUE + 1
+    t.tln(f" * the global value is {global_box.value}")
+    global_box.value = global_box.value + 1
     t.tln(f" * increased it")
-    t.tln(f" * the global value is now {THE_GLOBAL_VALUE}")
+    t.tln(f" * the global value is now {global_box.value}")
     t.tln(f" * sleeping 100 ms")
     sleep(0.1)
-    t.tln(f" * the global value is now {THE_GLOBAL_VALUE}")
+    t.tln(f" * the global value is now {global_box.value}")
     t.tln(f" * decreased it")
-    THE_GLOBAL_VALUE = THE_GLOBAL_VALUE - 1
-    t.tln(f" * the global value is now {THE_GLOBAL_VALUE}")
+    global_box.value = global_box.value - 1
+    t.tln(f" * the global value is now {global_box.value}")
 
 
-@bt.depends_on(Resource("the_global_value"))
-def test_resource_use_1(t: bt.TestCaseRun):
-    t_resource_use_with_race_condition(t)
+@bt.depends_on(bt.Resource(THE_GLOBAL_BOX))
+def test_resource_use_1(t: bt.TestCaseRun, global_box):
+    t_resource_use_with_race_condition(t, global_box)
 
 
-@bt.depends_on(Resource("the_global_value"))
-def test_resource_use_2(t: bt.TestCaseRun):
-    t_resource_use_with_race_condition(t)
+@bt.depends_on(bt.Resource(THE_GLOBAL_BOX))
+def test_resource_use_2(t: bt.TestCaseRun, global_box):
+    t_resource_use_with_race_condition(t, global_box)
 
 
-@bt.depends_on(Resource("the_global_value"))
-def test_resource_use_3(t: bt.TestCaseRun):
-    t_resource_use_with_race_condition(t)
+@bt.depends_on(bt.Resource(THE_GLOBAL_BOX))
+def test_resource_use_3(t: bt.TestCaseRun, global_box):
+    t_resource_use_with_race_condition(t, global_box)
